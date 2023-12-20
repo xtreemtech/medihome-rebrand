152bc7f1348916380e6464ee6840483392e8084c
Eddybest674 <edidiongeffiong056@gmail.com>
2023-12-20 13:27:53 +0000

    eddy

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,191 @@
 <x-landing-layout>
 
     <x-shortbanner title="About">
-        
     </x-shortbanner>
+    <div class="container mx-auto">
+        <div class="grid m-5 grid-cols-1">
+            <div class="lg:col-span-12">
+                <p class="w-70" > MEDIHOME Care Services are passionate about delivering quality services of an extremely high standard as if it were our loved ones receiving care and support.<br>
+                    Allow our multidisciplinary Medihome team provide in-house services for your loved ones.<br>
+                    During the Coronavirus pandemic, especially the lockdowns, we were able to provide outstanding quality care for our customers in accordance with covid protocol, both existing and new.<br> We ensured staff are fully protected against the Coronavirus, with personal protective clothing, sanitizer, weekly covid tests, training, policies ...<br>
+                    No Wonder MEDIHOME is recognised as one of the best and top home care providers. We have a proven track records, reputation and are rated as consistent by Homecare service providers in Nigeria.
+                </p>
+            </div>
+    
+             
+        </div>
+      
+    </div>
+    <hr><!--
+    <div class="container mx-auto mt-10">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+    
+            <div class="bg-emerald-500 p-4">
+                <h3>MEDIHOME TEAM &amp; STAFF STRENGTH</h3>
+                <p class="text-white">
+                    Our medical team majorly consists of dedicated professionals:
+                </p>
+                <ul>
+                    <li>High rank medical Consultants from different specialties</li>
+                    <li>Specialist Nurses</li>
+                    <li>Physical therapists</li>
+                    <li>Laboratory scientists</li>
+                    <li>Clinical Pharmacists</li>
+                    <li>Dieticians</li>
+                    <li>Professional Caregivers</li>
+                </ul>
+    
+                <div class="grid grid-cols-1">
+                    <h3>OUR MISSION</h3>
+                    
+                    <p>
+                        To deliver high-quality care to improve and maintain a healthy quality of life for all people while using a holistic person-centered care approach in the delivery of medical and non-medical home care, social services, and supports.
+                    </p>
+                </div>
+            </div>
+    
+            <div class="bg-yellow-800 p-4">
+                <h3>OUR VISION</h3>
+                <p>
+                    To become a world-class leader and preferred home care organization by continually evaluating and improving the way we conduct ourselves, ensuring our patients always receive exceptional services and care.
+                </p>
+            </div>
+    
+        </div>
+    </div>
+    <div class="container mx-auto mt-10">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+    
+            
+            <div class="bg-emerald-500 p-6 rounded-md shadow-md hover:shadow-lg transition duration-300 ease-in-out">
+                <h3 class="text-white text-2xl font-bold mb-4">MEDIHOME TEAM & STAFF STRENGTH</h3>
+                <p class="text-white mb-4">
+                    Our medical team consists of dedicated professionals:
+                </p>
+                <ul class="text-white list-disc pl-6">
+                    <li>High rank medical Consultants from different specialties</li>
+                    <li>Specialist Nurses</li>
+                    <li>Physical therapists</li>
+                    <li>Laboratory scientists</li>
+                    <li>Clinical Pharmacists</li>
+                    <li>Dieticians</li>
+                    <li>Professional Caregivers</li>
+                </ul>
+            </div>
+    
+        </div>
+    </div>-->
+    <div class="container mx-auto mt-10">
+     <div class=" mt-10 p-8">
+
+        <!-- Team & Staff Strength Section -->
+        <div class="bg-blue-700 p-20 rounded-md shadow-md hover:shadow-lg transition duration-300 ease-in-out">
+            <h3 class="text-white text-center text-2xl font-bold mb-4">MEDIHOME TEAM & STAFF STRENGTH</h3>
+            <p class="text-white text-center mb-4">
+                Our medical team consists of dedicated professionals:
+            </p>
+            <ul class="text-white list-none p-0 lg:grid grid-cols-4 sm:grid-cols-2 gap-8">
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">High rank medical Consultants</h4>
+                    <p class="text-gray-700">Our team includes top-tier medical consultants, each specializing in various medical fields, ensuring comprehensive expertise for our patients.</p>
+                </li>
+            
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">Specialist Nurses</h4>
+                    <p class="text-gray-700">Our dedicated nursing staff comprises specialists who provide personalized care and attention to meet the unique needs of each patient.</p>
+                </li>
+            
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">Physical therapists</h4>
+                    <p class="text-gray-700">Our skilled physical therapists work closely with patients to enhance their physical well-being through tailored rehabilitation programs and exercises.</p>
+                </li>
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">Specialist Nurses</h4>
+                    <p class="text-gray-700">Our dedicated nursing staff comprises specialists who provide personalized care and attention to meet the unique needs of each patient.</p>
+                </li>
+            
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">Laboratory Scientist</h4>
+                    <p class="text-gray-700">Our skilled laboratory scientists play a crucial role in conducting thorough and accurate medical tests, contributing to precise diagnoses and effective treatment plans.</p>
+                </li>
+            
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">Clinical Pharmacist</h4>
+                    <p class="text-gray-700">Our clinical pharmacists collaborate with the medical team to ensure safe and effective medication management, tailoring prescriptions to each patient's unique needs.</p>
+                </li>
+            
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">Dietician</h4>
+                    <p class="text-gray-700">Our dedicated dieticians provide personalized nutritional guidance, helping patients achieve and maintain optimal health through carefully crafted diet plans.</p>
+                </li>
+            
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">Professional Caregivers</h4>
+                    <p class="text-gray-700">Our compassionate professional caregivers offer attentive and supportive care to patients, ensuring their comfort and well-being throughout their healthcare journey.</p>
+                </li>
+            
+                
+            </ul>
+            
+        </div>
+
+    </div>
+</div>
+
+    
+
+<div class="container mx-auto mt-10">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+
+        
+        <div class="bg-gray-100 sm:col-start-2 md:col-start-2 m-50 p-6 rounded-md">
+            <h3 class="text-black text-center text-xl font-bold mb-2">OUR VISION</h3>
+            <p class="text-black">
+                To become a world-class leader and preferred home care organization by continually evaluating and improving the way we conduct ourselves, ensuring our patients always receive exceptional services and care.
+            </p>
+        </div>
+
+        
+        <div class="sm:col-start-2 md:col-start-3 bg-gray-100 p-6 m-50 rounded-md">
+            <div class="mt-6">
+                <h3 class="text-black text-center text-lg font-bold mb-2">OUR MISSION</h3>
+                <p class="text-black">
+                    To deliver high-quality care to improve and maintain a healthy quality of life for all people while using a holistic person-centered care approach in the delivery of medical and non-medical home care, social services, and supports.
+                </p>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+  
+
+    <div class="bg-gray-100 mt-10 p-8">
+        <h3 class="text-center text-gray-800 text-2xl font-bold mb-4">Our Values</h3>
+        <p class="text-center text-gray-600 mb-8">MediHome's Standard of Excellence.</p>
+    
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+            <div class="bg-blue-700 p-8 text-center text-white rounded-lg">
+                <img class="w-24 mx-auto mb-4" src="{{ asset('/icons/emp.svg') }}" alt="Empathy Icon">
+                <p class="font-semibold">Empathy</p>
+            </div>
+    
+            <div class="bg-blue-700 p-8 text-center text-white rounded-lg">
+                <img class="w-24 mx-auto mb-4" src="{{ asset('/icons/respect.svg') }}" alt="Respect Icon">
+                <p class="font-semibold">Respect</p>
+            </div>
+    
+            <div class="bg-blue-700 p-8 text-center text-white rounded-lg">
+                <img class="w-24 mx-auto mb-4" src="{{ asset('/icons/handshake.svg') }}" alt="Integrity Icon">
+                <p class="font-semibold">Integrity</p>
+            </div>
+    
+            <div class="bg-blue-700 p-8 text-center text-white rounded-lg">
+                <img class="w-24 mx-auto mb-4" src="{{ asset('/icons/innovation.svg') }}" alt="Innovation Icon">
+                <p class="font-semibold">Innovation</p>
+            </div>
+        </div>
+    </div>
 
 </x-landing-layout>
