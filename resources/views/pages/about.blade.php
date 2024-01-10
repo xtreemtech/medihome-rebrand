d566c59d13c10512b7f26464faa6f4e4054d4669
Eddybest674 <edidiongeffiong056@gmail.com>
2024-01-10 13:51:41 +0000

    updated

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,88 +3,105 @@
     <x-shortbanner title="About">
     </x-shortbanner>
     <div class="container mx-auto">
-        <div class="grid m-5 grid-cols-1">
+        <div class="grid grid-cols-1">
             <div class="lg:col-span-12">
-                <p class="w-70" > MEDIHOME Care Services are passionate about delivering quality services of an extremely high standard as if it were our loved ones receiving care and support.<br>
-                    Allow our multidisciplinary Medihome team provide in-house services for your loved ones.<br>
-                    During the Coronavirus pandemic, especially the lockdowns, we were able to provide outstanding quality care for our customers in accordance with covid protocol, both existing and new.<br> We ensured staff are fully protected against the Coronavirus, with personal protective clothing, sanitizer, weekly covid tests, training, policies ...<br>
-                    No Wonder MEDIHOME is recognised as one of the best and top home care providers. We have a proven track records, reputation and are rated as consistent by Homecare service providers in Nigeria.
-                </p>
+                <p class="w-70 text-lg text-gray-800 leading-loose">MEDIHOME Care Services is passionate about delivering quality services of an extremely high standard as if it were our loved ones receiving care and support. Allow our multidisciplinary Medihome team to provide in-house services for your loved ones.
+                    <br> During the Coronavirus pandemic, especially the lockdowns, we were able to provide outstanding quality care for our customers in accordance with COVID protocol, both existing and new.
+                    <br> We ensured staff are fully protected against the Coronavirus, with personal protective clothing, sanitizer, weekly COVID tests, training, policies... No Wonder MEDIHOME is recognized as one of the best and top home care providers. We have a proven track record, reputation, and are rated as consistent by Homecare service providers in Nigeria.</p>
             </div>
-    
-             
-        </div>
-      
-    </div>
-    <hr><!--
-    <div class="container mx-auto mt-10">
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-    
-            <div class="bg-emerald-500 p-4">
-                <h3>MEDIHOME TEAM &amp; STAFF STRENGTH</h3>
-                <p class="text-white">
-                    Our medical team majorly consists of dedicated professionals:
-                </p>
-                <ul>
-                    <li>High rank medical Consultants from different specialties</li>
-                    <li>Specialist Nurses</li>
-                    <li>Physical therapists</li>
-                    <li>Laboratory scientists</li>
-                    <li>Clinical Pharmacists</li>
-                    <li>Dieticians</li>
-                    <li>Professional Caregivers</li>
-                </ul>
-    
-                <div class="grid grid-cols-1">
-                    <h3>OUR MISSION</h3>
-                    
-                    <p>
-                        To deliver high-quality care to improve and maintain a healthy quality of life for all people while using a holistic person-centered care approach in the delivery of medical and non-medical home care, social services, and supports.
-                    </p>
-                </div>
-            </div>
-    
-            <div class="bg-yellow-800 p-4">
-                <h3>OUR VISION</h3>
-                <p>
-                    To become a world-class leader and preferred home care organization by continually evaluating and improving the way we conduct ourselves, ensuring our patients always receive exceptional services and care.
-                </p>
-            </div>
-    
         </div>
     </div>
-    <div class="container mx-auto mt-10">
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
     
-            
-            <div class="bg-emerald-500 p-6 rounded-md shadow-md hover:shadow-lg transition duration-300 ease-in-out">
-                <h3 class="text-white text-2xl font-bold mb-4">MEDIHOME TEAM & STAFF STRENGTH</h3>
-                <p class="text-white mb-4">
-                    Our medical team consists of dedicated professionals:
-                </p>
-                <ul class="text-white list-disc pl-6">
-                    <li>High rank medical Consultants from different specialties</li>
-                    <li>Specialist Nurses</li>
-                    <li>Physical therapists</li>
-                    <li>Laboratory scientists</li>
-                    <li>Clinical Pharmacists</li>
-                    <li>Dieticians</li>
-                    <li>Professional Caregivers</li>
-                </ul>
-            </div>
     
-        </div>
-    </div>-->
-    <div class="container mx-auto mt-10">
-     <div class=" mt-10 p-8">
+<div class="container mx-auto mt-10">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
 
-        <!-- Team & Staff Strength Section -->
-        <div class="bg-blue-700 p-20 rounded-md shadow-md hover:shadow-lg transition duration-300 ease-in-out">
-            <h3 class="text-white text-center text-2xl font-bold mb-4">MEDIHOME TEAM & STAFF STRENGTH</h3>
-            <p class="text-white text-center mb-4">
+        <div class="bg-blue-200 sm:col-start-2 md:col-start-2 m-5 p-6 rounded-md">
+            <h3 class="text-white text-center text-lg font-bold mb-2">OUR VISION</h3>
+            <p class="text-white">
+                To become a world-class leader and preferred home care organization by continually evaluating and improving the way we conduct ourselves, ensuring our patients always receive exceptional services and care.
+            </p>
+        </div>
+
+        <div class="bg-green-200 sm:col-start-2 md:col-start-3 p-6 m-5 rounded-md">
+            <div class="mt-6">
+                <h3 class="text-white text-center text-lg font-bold mb-2">OUR MISSION</h3>
+                <p class="text-white">
+                    To deliver high-quality care to improve and maintain a healthy quality of life for all people while using a holistic person-centered care approach in the delivery of medical and non-medical home care, social services, and supports.
+                </p>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<div class="mx-auto mt-10">
+    <div class="mt-10 p-8 bg-gray-300 rounded-md">
+
+        <div class="w-full p-20 rounded-md">
+            <h3 class="text-gray-700 text-center text-2xl font-bold mb-4">MEDIHOME TEAM & STAFF STRENGTH</h3>
+            <p class="text-gray-700 text-center mb-5">
                 Our medical team consists of dedicated professionals:
             </p>
-            <ul class="text-white list-none p-0 lg:grid grid-cols-4 sm:grid-cols-2 gap-8">
+            <ul class="text-white list-none mt-5 p-0 lg:grid grid-cols-4 sm:grid-cols-2 gap-8">
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">✓  Medical Consultants</h4>
+                    <p class="text-gray-700"></p>
+                </li>
+            
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">✓ Specialist Nurses</h4>
+                    <p class="text-gray-700"></p>
+                </li>
+            
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">✓ Physical therapists</h4>
+                    <p class="text-gray-700"></p>
+                </li>
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">✓ Specialist Nurses</h4>
+                    <p class="text-gray-700"></p>
+                </li>
+            
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">✓ Laboratory Scientist</h4>
+                    <p class="text-gray-700"></p>
+                </li>
+            
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">✓ Clinical Pharmacist</h4>
+                    <p class="text-gray-700"></p>
+                </li>
+            
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">✓ Professional Caregivers</h4>
+                    <p class="text-gray-700"></p>
+                </li>
+            
+                <li class="mb-2 bg-white p-4 rounded-md">
+                    <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">✓ Dietician </h4>
+                    <p class="text-gray-700"></p>
+                </li>
+            
+            </ul>
+        </div>
+
+    </div>
+</div>
+
+
+<!--    
+    <div class=" mx-auto mt-10">
+     <div class=" mt-10 p-8">
+
+        
+        <div class="  w-full p-20 rounded-md ">
+            <h3 class="text-gray-700 text-center text-2xl font-bold mb-4">MEDIHOME TEAM & STAFF STRENGTH</h3>
+            <p class="text-gray-700 text-center mb-5">
+                Our medical team consists of dedicated professionals:
+            </p>
+            <ul class="text-white list-none mt-5 p-0 lg:grid grid-cols-4 sm:grid-cols-2 gap-8">
                 <li class="mb-2 bg-white p-4 rounded-md">
                     <h4 class="text-lg text-center text-gray-800 text-2xl font-bold mb-4 font-semibold mb-2">High rank medical Consultants</h4>
                     <p class="text-gray-700">Our team includes top-tier medical consultants, each specializing in various medical fields, ensuring comprehensive expertise for our patients.</p>
@@ -129,34 +146,12 @@
             
         </div>
 
+
     </div>
-</div>
+</div>  -->
 
     
 
-<div class="container mx-auto mt-10">
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-
-        
-        <div class="bg-gray-100 sm:col-start-2 md:col-start-2 m-50 p-6 rounded-md">
-            <h3 class="text-black text-center text-xl font-bold mb-2">OUR VISION</h3>
-            <p class="text-black">
-                To become a world-class leader and preferred home care organization by continually evaluating and improving the way we conduct ourselves, ensuring our patients always receive exceptional services and care.
-            </p>
-        </div>
-
-        
-        <div class="sm:col-start-2 md:col-start-3 bg-gray-100 p-6 m-50 rounded-md">
-            <div class="mt-6">
-                <h3 class="text-black text-center text-lg font-bold mb-2">OUR MISSION</h3>
-                <p class="text-black">
-                    To deliver high-quality care to improve and maintain a healthy quality of life for all people while using a holistic person-centered care approach in the delivery of medical and non-medical home care, social services, and supports.
-                </p>
-            </div>
-        </div>
-
-    </div>
-</div>
 
 
   
@@ -186,6 +181,7 @@
                 <p class="font-semibold">Innovation</p>
             </div>
         </div>
-    </div>
+    </div> 
+    
 
 </x-landing-layout>
