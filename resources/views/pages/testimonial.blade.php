230786c90cc788190b33006a6f44b40bdfe2be83
Nicholas chuks <niccobery91@gmail.com>
2024-01-20 22:35:09 +0100

    started the testimonial page

--- resources/views/pages/testimonial.blade.php
+++ resources/views/pages/testimonial.blade.php
@@ -3,6 +3,74 @@
     <x-shortbanner title="Testimonials">
         <h1>HOME CARE AGENCY</h1>
     </x-shortbanner>
-    
+ 
+  <div class="max-w-[1150px] mx-auto p-4 bg-blue-50 py-12 grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div class="border-r-2 border-sky-200 p-4 rounded-lg">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">George Omotese</h2>
+      <p class="text-lg mt-6 text-sky-800">MHCS Medicare was the second team for my late father. They cooperated with me. They understood his issues, they totally supported him and me right up to the end. I will forever feel grateful. You helped me keep him where he wanted to be, and you know how to play along with him always. Kudos!! The contract then started around February 2018, until he sadly passed away in May 2021 at age of 87yrs. I can honestly say they became like family to us and still are. Their standard of care is amazing but more than that they really do put patient first. I salute the MD & His team for professionalism indeed.</p>
     </div>
+
+     <div class="border-r-2 border-sky-200 p-4 rounded-lg">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">Stephen Paul</h2>
+      <p class="text-lg mt-6 text-sky-800">Following an unplanned stay in hospital which resulted in two procedures taking place, it was suggested that I should consider some personal care for a few weeks until I recovered from my ordeal. As a family, we connected the Director of MediHome Care Services through a Nurse "Matron", for several years whilst they cared for my parent. They connected other professionals when need arises without further delay. No unnecessary hospital visit again. Great, superb & Wonderful Services indeed. I recommend MediHome.</p>
+    </div>
+
+
+        <div class="border-r-2 border-sky-200 p-4 rounded-lg">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">Alhaja Muibat O.</h2>
+      <p class="text-lg mt-6 text-sky-800">My mother was admitted into hospital, terminal case. After 37 days, the management told us to look elsewhere because of NARD Strike. A relative of another patient told us about MediHome, I called them & we quickly subscribed likewise. We adhered to their B & D packages and behold, they gave us all their packages without extra charges.The Consultant visited her times without number. The carer were polite in their duty, Nurses connected well with Doctor. My mother was perfectly okay within 4 months. Honestly, MediHome is another multispecialist hospital within our home. Best healthcare Innovation Services. Hail MHCS!!!!!</p>
+    </div>
+
+    <div class="border-r-2 border-sky-200 p-4 rounded-lg"">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">Awolu Yaya</h2>
+      <p class="text-lg mt-6 text-sky-800">Wonderful service to Mum in her home. We found all carers and management team very supportive, listened to what we said, and particularly responsive and caring to Mum. I cannot recommend them highly enough.</p>
+    </div>
+    <div class="border-r-2 border-sky-200 p-4 rounded-lg">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">Ps. Solomon Adeniran</h2>
+      <p class="text-lg mt-6 text-sky-800">MediHome care services visited my father daily (and more by request of a phone call as necessary). They are all very nice, skilled and cheerful people, who really care. They also take time to connect other Specialist when the need arise and have a conversation that promotes the wellbeing of their patients. I sincerely recommend them.</p>
+    </div>
+    <div class="border-r-2 border-sky-200 p-4 rounded-lg">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">Kahdijat Hussien</h2>
+      <p class="text-lg mt-6 text-sky-800">Wonderful service to Mum in her home. We found all carers and management team very supportive, listened to what we said, and particularly responsive and caring to Mum. I cannot recommend them highly enough; something we couldn't always do with previous caring team. Effortless triumph.</p>
+    </div>
+    <div class="border-r-2 border-sky-200 p-4 rounded-lg">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">Ofor Blessing</h2>
+      <p class="text-lg mt-6 text-sky-800">My mother has been through some care companies - six or seven of them - and I can say without any hesitation that MediHome care Services is the best care company we could have wished for. The carers from MEDIHOME are second to none. All my siblings, we never worry about Mama's challenge since we engaged MediHome. Though she's not an easy old woman but they won her heart. Kudos to you all.</p>
+    </div>
+    <div class="border-r-2 border-sky-200 p-4 rounded-lg">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">Sobowale magret</h2>
+      <p class="text-lg mt-6 text-sky-800">I am happy to depend on their care and assistance. Interestingly, the team leader is always in contact, checking the Patient & Carers as well. They are mega home care professionals & Specialist indeed. I have already recommended this company to my friends both home and abroad. What a special team. Thank you all at MEDIHOME!!</p>
+    </div>
+    <div class="border-r-2 border-sky-200 p-4 rounded-lg">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">Silas Kutunga</h2>
+      <p class="text-lg mt-6 text-sky-800">We are really very satisfied with their Caregivers Services all through. Both Doctor & Nurse follow-up the care plan as stipulated. We couldn't get such wonderful care while he was admitted. We would have no hesitation to recommend MediHome Care Services to everyone and sure lots of people would be very happy to have your undisputed care and assistance. The entire family salute your passion!!!</p>
+    </div>
+    <div class="border-r-2 border-sky-200 p-4 rounded-lg">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">Wakel & Barakat Mayaki</h2>
+      <p class="text-lg mt-6 text-sky-800">MHCS are a very professional organisation. Their staff are very friendly, well-trained, smart and presentable. They always follow the guidelines for care during the covid19 pandemic. They have proven to be very professional, Reliable and ready listen to their client's needs and do their best. MHCS is overall best!!!!</p>
+    </div>
+    <div class="border-r-2 border-sky-200 p-4 rounded-lg">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">Morayo Afolabi</h2>
+      <p class="text-lg mt-6 text-sky-800">I felt it is pertinent to confirm that MHCS displays outstanding levels of care, sensitivity and a strong partnership approach. I unreservedly recommend them as all team members highlight the importance of recognising and respecting the requirements of the individual person they care for. The team deserves an award!!!</p>
+    </div>
+    <div class="border-r-2 border-sky-200 p-4 rounded-lg">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">Chuwkuka Ebere</h2>
+      <p class="text-lg mt-6 text-sky-800">Giving a level of care and service above and beyond that which was expected. Exceptional care and attention is always perfect. Very pleased and grateful. MediHome care Services is the way.</p>
+    </div>
+    <div class="border-r-2 border-sky-200 p-4 rounded-lg">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">Babalola Adetayo</h2>
+      <p class="text-lg mt-6 text-sky-800">My father really looks forward to the visits from MHCS caregivers. They are kind, cheerful and professional. They assisted my father to recover from his eye disorder & Parkinson's disease, moving around the house now without much assistance. They have perfect means of sourcing Drugs through their Clinical pharmacist and delivered at appropriate time except unprescribed from opthalmologist. Excellence service throughout. The best home care provider here in Nigeria. Alasss MHCS!!!</p>
+    </div>
+
+    <div class="border-r-2 border-sky-200 p-4 rounded-lg">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">Oyelade titilayo</h2>
+      <p class="text-lg mt-6 text-sky-800">Since taken over my mother's care, she was better and sounded happier. The multidisciplinary Team are fantastic people. Most importantly, they listen to my mum's needs every point in time. Care as it should be. I totally recommend this company.</p>
+    </div>
+    <div class="border-r-2 border-sky-200 p-4 rounded-lg">
+      <h2 class="text-center text-sky-800 text-3xl font-medium relative after:absolute after:-bottom-2 after:left-[50%] after:h-[4px] after:w-[80px] after:-translate-x-[50%] after:bg-blue-600 after:content-['']">Afred Obaloluwa</h2>
+      <p class="text-lg mt-6 text-sky-800">Fantastic in timing no matter odds. Seeing them alone is another healing, the said re-corporate package is top notch. My inlaw fully recovered within 2 months of their services. MHCS No option!!!</p>
+    </div> 
+  </div>
+
+    <!-- </div> -->
 </x-landing-layout>
