283fb4649bd91ca8e0569366421c0002b4572328
Nicholas chuks <niccobery91@gmail.com>
2024-02-24 17:15:35 +0100

    fix the testimonial page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,11 +5,13 @@
       <h1 class="text-center text-white text-5xl">About</h1>
     </div> 
  <!-- rounded-t-[40px] -->
-    <div class="w-full p-10 mt-6 bg-[url(https://9mobile.com.ng/_next/static/media/testimonialBackground.4bd99407.png)] bg-no-repeat bg-cover rounded-[40px] max-w-[1200px] w-[90%] mx-auto">
-<h3 class="text-xl md:text-2xl text-black leading-9 md:leading-[40px]">MEDIHOME Care Services are passionate about delivering quality services of an extremely high standard as if it were our loved ones receiving care and support. Allow our multidisciplinary Medihome team provide in-house services for your loved ones. During the Coronavirus pandemic, especially the lockdowns, we were able to provide outstanding quality care for our customers in accordance with covid protocol, both existing and new. We ensured staff are fully protected against the Coronavirus, with personal protective clothing, sanitizer, weekly covid tests, training, policies ... No Wonder MEDIHOME is recognised as one of the best and top home care providers. We have a proven track records, reputation and are rated as consistent by Homecare service providers in Nigeria.</h3>
+    <div style="background-image: url('{{ asset('images/banner/light-bg.jpg') }}');" class="w-full p-10 mt-6 bg-no-repeat bg-cover rounded-[40px] max-w-[1200px] w-[90%] mx-auto">
+<h3 class="text-xl md:text-2xl text-black leading-9 md:leading-[40px]">MEDIHOME Care Services are passionate about delivering quality services of an extremely high standard as if it were our loved ones receiving care and support. Allow our multidisciplinary Medihome team provide in-house services for your loved ones. </h3>
+<h3 class="text-xl md:text-2xl text-black leading-9 md:leading-[40px]"> During the Coronavirus pandemic, especially the lockdowns, we were able to provide outstanding quality care for our customers in accordance with covid protocol, both existing and new. We ensured staff are fully protected against the Coronavirus, with personal protective clothing, sanitizer, weekly covid tests, training, policies ... </h3> 
+<h3 class="text-xl md:text-2xl text-black leading-9 md:leading-[40px]">No Wonder MEDIHOME is recognised as one of the best and top home care providers. We have a proven track records, reputation and are rated as consistent by Homecare service providers in Nigeria.</h3>
     </div>
 
-    <div class="bg-[url(https://9mobile.com.ng/_next/static/media/offer-background.9d0375c7.svg)] bg-no-repeat bg-cover p-10 mt-6 mx-auto max-w-[1200px] w-[90%] mx-auto rounded-[40px] grid grid-cols-1 md:grid-cols-2 gap-6">
+    <div class="p-10 mt-6 mx-auto h-full max-w-[1200px] w-[90%] mx-auto rounded-[40px] grid grid-cols-1 md:grid-cols-2 gap-6">
         <div>
             <div class="bg-[#f0f8ff] mb-10 p-8 rounded-[20px]">
                 <h3 class="text-2xl mb-6 text-black">MEDIHOME TEAM & STAFF STRENGTH</h3>
@@ -24,39 +26,22 @@
                     <li class="text-xl mb-2">Professional Caregivers</li>
                 </ul>
             </div>
-
-
-            <div class="bg-[#fff2ee] mb-10 p-[20px] rounded-[20px] text-center">
-                <h3 class="text-2xl mb-2">OUR MISSION</h3>
-                <p class="text-black text-[20px] leading-[30px]">To deliver high quality care to improve and maintain a healthy quality of life for all people while using a holistic person-centered care approach in the delivery of medical and non-medical home care, social services and supports.</p>
-            </div>
-
         </div>
 
         <div>
-
-            <div class="bg-[#fff2ee] mb-12 p-[20px] rounded-[20px] text-center">
+                <div class="bg-[#f0f8ff] mb-10 p-[20px] rounded-[20px] text-center">
+                <h3 class="text-2xl mb-2">OUR MISSION</h3>
+                <p class="text-black text-[20px] leading-[30px]">To deliver high quality care to improve and maintain a healthy quality of life for all people while using a holistic person-centered care approach in the delivery of medical and non-medical home care, social services and supports.</p>
+            </div>
+            <div class="bg-[#f0f8ff] mb-12 p-[20px] rounded-[20px] text-center">
                 <h3 class="text-2xl mb-2">OUR VISION</h3>
                 <p class="text-black text-[20px] leading-[30px]">To become world class leader and preferred home care organization by continually evaluating improving the way we conduct ourselves, ensuring our patients always receive exceptional services and care.</p>
             </div>
-            <div class="bg-[#fff2ee] p-[20px] rounded-[20px] text-center">
-                <h3 class="text-2xl mb-2">OUR VALUES</h3>
-                <p class="text-black text-[20px] leading-[30px]">MediHome's standard of Excellence.</p>
-                <div>
-                    <div class="flex place-content-center gap-6 text-center mt-6">
-                        <span class="w-[120px] bg-white text-[20px] text-black p-4 rounded-lg text-center">Empathy</span>
-                        <span class="w-[120px] bg-white text-[20px] text-black p-4 rounded-lg text-center">Respect</span>
-                    </div>
-                    <div class="flex place-content-center gap-6 text-center mt-4 mb-2">
-                        <span class="w-[120px] bg-white text-[20px] text-black p-4 rounded-lg text-center">Integrity</span>
-                        <span class="w-[120px] bg-white text-[20px] text-black p-4 rounded-lg text-center">Innovation</span>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     </div>
 
-<div class="bg-[url(https://9mobile.com.ng/_next/static/media/data-sharing-bg-img.b234fb4d.svg)] bg-no-repeat bg-cover p-10 mt-10 mb-10 mx-auto max-w-[1200px] w-[90%] mx-auto rounded-[40px] text-white">
+<div class="bg-no-repeat bg-cover p-10 mt-10 mb-10 mx-auto max-w-[1200px] w-[90%] mx-auto rounded-[40px] text-white" style="background-image: url('{{ asset('images/banner/dark-bg.svg') }}');" >
     <h3 class="text-center text-2xl mb-2">SERVICES</h3>
     <h5 class="text-center text-xl mb-3">HOME CARE AGENCY</h5>
     <p class="text-[18px] leading-[30px]">MEDIHOME is a licensed and certified home care Agency through Ministry Of Health and also Incorporated at corporate Affairs Commission here in Nigeria. Our organization is involved in providing home health, companionship & social care services to individuals in their places of residence either temporary or permanent settlements.
@@ -87,14 +72,14 @@ Our in-Services are but not limited to :</p>
         
     </div>
     <div class="grid grid-cols-2 lg:grid-cols-4 gap-3 mt-4">
-       <div class="bg-white text-[15px] md:text-[20px] text-black p-2 rounded-lg text-center"><span>✓ Palliative & Hospice Care</span></div>
+       <div class="bg-white text-[15px] md:text-[20px] text-black p-2 rounded-lg text-center"><span>✓ Respite cover</span></div>
        <div class="bg-white text-[15px] md:text-[20px] text-black p-2 rounded-lg text-center"><span>✓ Personal Hygiene Care</span></div>
        <div class="bg-white text-[15px] md:text-[20px] text-black p-2 rounded-lg text-center"><span>✓ Physical Disability</span></div>
        <div class="bg-white text-[15px] md:text-[20px] text-black p-2 rounded-lg text-center"><span>✓ Medical Social Work</span></div>
     </div>
-    <div class="grid grid-cols-2 lg:grid-cols-4 gap-3 mt-4">
+    <div class="grid grid-cols-1 gap-3 mt-4">
         <div class="bg-white text-[15px] md:text-[20px] text-black p-2 rounded-lg text-center"><span>✓ Blood Transfusion & related services</span></div>
-        <div class="bg-white text-[15px] md:text-[20px] text-black p-2 rounded-lg text-center"><span>✓ Respite cover</span></div>
+        
     </div>
 </div>
 
