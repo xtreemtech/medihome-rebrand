73cfb67ca2692530da58de38c22b76dac5dbb6cd
xtreemtech <visitmrdaniel@gmail.com>
2024-03-23 20:24:06 +0100

    update

--- resources/views/pages/errors/notfound.blade.php
+++ resources/views/pages/errors/notfound.blade.php
@@ -1,10 +1,18 @@
 <x-landing-layout>
-
-    <x-shortbanner title="MTN">
-        
-    </x-shortbanner>
-    <div>
-        <div>
-        Not found Page (Error 404)
+<div class="bg-dark d-flex align-items-center justify-content-center min-vh-100">
+    <div class="container">
+        <div class="p-5 m-5 text-center">
+            <div class="card p-5 rounded-5">
+                <h1 class="display-1 fw-bold">401</h1>
+                <h4 class="display-6">Unauthorized Access</h4>
+                <p>
+                You are not authorized to access this page.
+                </p>
+                <div>
+                <a href="{{ url('/') }}" class="btn btn-primary rounded-5 px-5">Go Back Home</a>
+</div>
+            </div>
+        </div>
     </div>
+</div>
 </x-landing-layout>
